eab1e634a2c6715a29e670183acbea9362a9aec3
Yevhenii Liashenko <paradoxrial@gmail.com>
2024-03-19 19:38:18 +0200

    book service

--- src/main/java/store/bookshop/BookshopApplication.java
+++ src/main/java/store/bookshop/BookshopApplication.java
@@ -1,13 +1,32 @@
 package store.bookshop;
 
+import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import store.bookshop.model.Book;
+import store.bookshop.service.BookService;
 
 @SpringBootApplication
 public class BookshopApplication {
+    @Autowired
+    private BookService bookService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookshopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookshopApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book book = new Book("Title 1", "Author 1", "ISBN1", BigDecimal.valueOf(19.99));
+            book.setDescription("description 1");
+            book.setCoverImage("cover1.jpg");
+            bookService.save(book);
+
+            System.out.println(bookService.findAll());
+        };
+    }
 }
