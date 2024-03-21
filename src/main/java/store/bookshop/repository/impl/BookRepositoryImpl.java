9d4522bb7fedb7f4048da8e5bcdf966c67f26a7c
Yevhenii Liashenko <paradoxrial@gmail.com>
2024-03-21 19:09:41 +0200

    update BookService and BookRepository

--- src/main/java/store/bookshop/repository/impl/BookRepositoryImpl.java
+++ src/main/java/store/bookshop/repository/impl/BookRepositoryImpl.java
@@ -1,6 +1,7 @@
 package store.bookshop.repository.impl;
 
 import java.util.List;
+import java.util.Optional;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -46,6 +47,16 @@ public class BookRepositoryImpl implements BookRepository {
             return session.createQuery("SELECT b FROM Book b", Book.class).getResultList();
         } catch (Exception e) {
             throw new RuntimeException("Can't get all books from DB", e);
+        }
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            Book book = session.get(Book.class, id);
+            return Optional.ofNullable(book);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find book with id " + id, e);
         }
     }
 }
--- src/main/java/store/bookshop/service/BookService.java
+++ src/main/java/store/bookshop/service/BookService.java
@@ -1,10 +1,13 @@
 package store.bookshop.service;
 
 import java.util.List;
-import store.bookshop.model.Book;
+import store.bookshop.dto.BookDto;
+import store.bookshop.dto.CreateBookRequestDto;
 
 public interface BookService {
-    Book save(Book book);
+    BookDto save(CreateBookRequestDto requestDto);
 
-    List<Book> findAll();
+    List<BookDto> findAll();
+
+    BookDto findById(Long id);
 }
