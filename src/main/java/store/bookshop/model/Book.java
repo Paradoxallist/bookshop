cd315fc9d4e88aa8e136ca437bd8e260e8ef5efe
Yevhenii Liashenko <paradoxrial@gmail.com>
2024-03-26 17:51:21 +0200

    update sql query in Book

--- src/main/java/store/bookshop/model/Book.java
+++ src/main/java/store/bookshop/model/Book.java
@@ -11,14 +11,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
-@Where(clause = "is_deleted=false")
+@SQLDelete(sql = "UPDATE books SET is_deleted = TRUE WHERE id = ?")
+@SQLRestriction(value = "is_deleted = FALSE")
 @Table(name = "books")
 public class Book {
     @Id
